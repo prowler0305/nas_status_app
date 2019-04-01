f44e355fdeba99d4f8908141a62db8a1fb31ca89
aspea002 <Andrew.Spear@uscellular.com>
2019-04-01 16:08:20 -0500

    New Feautre to all FAQ's to be deleted.

--- container_status_app/nas_add_faq.py
+++ container_status_app/nas_add_faq.py
@@ -42,25 +42,69 @@ class NasAddFaq(MethodView):
 
         read_json_rc, self.faq_dict = Common.rw_json_file(file_path=os.environ.get('faq_data_path'))
         if read_json_rc:
-            faq_category_dicts = self.faq_dict.get(request.form.get('faq_type_radio'))
-            if "\r\n" in request.form.get('faq_content'):
-                faq_content_data = request.form.get('faq_content').split("\r\n")
+            if 'deleteFAQButton' in request.form.keys():
+                    if self.delete():
+                        return render_template(self.nas_add_faq_html_template, faq_dict=self.faq_dict, faq_delete_rc=True)
+                    else:
+                        return render_template(self.nas_add_faq_html_template, faq_dict=self.faq_dict, faq_delete_rc=False)
             else:
-                faq_content_data = request.form.get('faq_content')
-            faq_category_dicts[request.form.get('faq_question')] = faq_content_data
-            self.faq_dict[request.form.get('faq_type_radio')] = faq_category_dicts
-            update_json_rc, file_updated = Common.rw_json_file(file_path=os.environ.get('faq_data_path'),
-                                                               mode='write',
-                                                               output_dict=self.faq_dict)
-            if update_json_rc:
-                container_status_app.logger.info("New FAQ: {} | Category: {} | Content: {}".format(
-                    request.form.get('faq_question'), request.form.get('faq_type_radio'), request.form.get('faq_content')))
-                return render_template(self.nas_add_faq_html_template, faq_dict=self.faq_dict, faq_added_rc=True)
-            else:
-                container_status_app.logger.error("Unable to add to Category: {} | FAQ: {} | Content: {}".format(
-                    request.form.get('faq_type_radio'), request.form.get('faq_question'), request.form.get('faq_content')))
-                return render_template(self.nas_add_faq_html_template, faq_added_rc=False)
-
+                faq_category_dicts = self.faq_dict.get(request.form.get('faq_type_radio'))
+                if "\r\n" in request.form.get('faq_content'):
+                    faq_content_data = request.form.get('faq_content').split("\r\n")
+                else:
+                    faq_content_data = request.form.get('faq_content')
+                faq_category_dicts[request.form.get('faq_question')] = faq_content_data
+                self.faq_dict[request.form.get('faq_type_radio')] = faq_category_dicts
+                update_json_rc, file_updated = Common.rw_json_file(file_path=os.environ.get('faq_data_path'),
+                                                                   mode='write',
+                                                                   output_dict=self.faq_dict)
+                if update_json_rc:
+                    container_status_app.logger.info("New FAQ: {} | Category: {} | Content: {}".format(
+                        request.form.get('faq_question'), request.form.get('faq_type_radio'), request.form.get('faq_content')))
+                    return render_template(self.nas_add_faq_html_template, faq_dict=self.faq_dict, faq_added_rc=True)
+                else:
+                    container_status_app.logger.error("Unable to add to Category: {} | FAQ: {} | Content: {}".format(
+                        request.form.get('faq_type_radio'), request.form.get('faq_question'), request.form.get('faq_content')))
+                    return render_template(self.nas_add_faq_html_template, faq_added_rc=False)
         else:
             container_status_app.logger.error("Unable to read JSON file containing FAQ data at path: {}".format(os.environ.get('faq_data_path')))
             return render_template(self.nas_add_faq_html_template, faq_added_rc=False)
+
+    def delete(self):
+        """
+        Called when the Delete button is pressed on the Delete FAQ form on the /nas/add/faq page.
+
+        Expects that the faq_dict class instance variable has been populated by the post() method above.
+
+        :return: True/False if the selected FAQ titles were deleted or not.
+        """
+        faqs_deleted = False
+        for faq_to_delete_key, faq_to_delete_value in request.form.items():
+            if 'FAQButton' in faq_to_delete_key:
+                continue
+            else:
+                faq_delete_metadata_list = faq_to_delete_key.split(':')
+                category_faq_dict = self.faq_dict.get(faq_delete_metadata_list[0])
+                if category_faq_dict.pop(faq_to_delete_value, None) is not None:
+                    container_status_app.logger.info("FAQ {} in Category {} has been deleted.".format(
+                        faq_to_delete_value, faq_delete_metadata_list[0].upper()))
+                    faqs_deleted = True
+                else:
+                    container_status_app.logger.warning("FAQ {} in Category {} not found.".format(
+                        faq_to_delete_value, faq_delete_metadata_list[0].upper()))
+                    faqs_deleted = False
+
+        update_json_rc, file_updated = Common.rw_json_file(file_path=os.environ.get('faq_data_path'),
+                                                           mode='write',
+                                                           output_dict=self.faq_dict)
+        if update_json_rc:
+            container_status_app.logger.info("FAQ data file {} updated successfully.".format(os.environ.get('faq_data_path')))
+        else:
+            container_status_app.logger.error("FAQ data file {} could not be updated.".format(os.environ.get('faq_data_path')))
+            if faqs_deleted:
+                faqs_deleted = False
+
+        if faqs_deleted:
+            return True
+        else:
+            return False
