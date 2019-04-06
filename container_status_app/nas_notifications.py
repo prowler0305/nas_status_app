7f4bc5faada0f018fdffb6b18766ff104dae66de
aspea002 <Andrew.Spear@uscellular.com>
2019-04-05 19:07:41 -0500

    Update notifications logic to be able to work with 2 NAS Platforms defined in the JSON file.

--- container_status_app/nas_notifications.py
+++ container_status_app/nas_notifications.py
@@ -46,20 +46,24 @@ class NasNotifications(MethodView):
         read_json_rc, self.container_status_dict = Common.rw_json_file(file_path=os.environ.get('container_status_path'))
         nas_production_dict = self.container_status_dict.get('nas_production')
 
-        if 'toggle_switch_form' in list(request.form.keys()):
+        if 'toggle_switch_form' in list(request.form.keys()) or 'p2_toggle_switch_form' in list(request.form.keys()):
             if nas_production_dict is not None and nas_production_dict.get('display_name') == 'NAS Automation Platform':
-                toggle_switch_names = []
-                for app_name in nas_production_dict.get('applications').keys():
-                    status_switch_name = app_name + '_status_switch'
-                    additional_info_name = request.form.get(app_name + '_textarea')
+                # toggle_switch_names = []
+                if 'toggle_switch_form' in list(request.form.keys()):
+                    toggle_switch_names = self.create_list_of_toggle_names(nas_production_dict.get('applications').keys())
+                    status_switch_name = nas_production_dict.get('display_name') + '_status_switch'
+                    additional_info_name = None
                     app_toggle_info_tuple = (status_switch_name, additional_info_name)
                     toggle_switch_names.append(app_toggle_info_tuple)
-                status_switch_name = nas_production_dict.get('display_name') + '_status_switch'
-                additional_info_name = None
-                app_toggle_info_tuple = (status_switch_name, additional_info_name)
-                toggle_switch_names.append(app_toggle_info_tuple)
+                    self.put(list_o_switch_name=toggle_switch_names)
+                if 'p2_toggle_switch_form' in list(request.form.keys()):
+                    toggle_switch_names = self.create_list_of_toggle_names(nas_production_dict.get('new_platform_apps').keys())
+                    status_switch_name = nas_production_dict.get('p2_display_name') + '_status_switch'
+                    additional_info_name = None
+                    app_toggle_info_tuple = (status_switch_name, additional_info_name)
+                    toggle_switch_names.append(app_toggle_info_tuple)
+                    self.put_p2(list_o_switch_name=toggle_switch_names)
 
-                self.put(list_o_switch_name=toggle_switch_names)
         else:
             if 'Choose' in request.form.get('time_of_day_start'):
                 return render_template(self.nas_notify_html_template, tods_error=True)
@@ -74,6 +78,16 @@ class NasNotifications(MethodView):
                 email_template_data = email_template_data.replace('{ed}', request.form.get('outage_end_date'))
                 email_template_data = email_template_data.replace('{et}', "{} {}".format(request.form.get('outage_end_time'), request.form.get('time_of_day_end')))
                 email_template_data = email_template_data.replace('{why}', "{}".format(request.form.get('reason_textarea')))
+                if request.form.get('platform_name') == 'old_platform':
+                    apps_list = nas_production_dict.get('applications').keys()
+                else:
+                    apps_list = nas_production_dict.get('new_platform_apps').keys()
+
+                app_replacement_string = ''
+                for app in apps_list:
+                    app_replacement_string = app_replacement_string + 'o	{}\n'.format(app)
+
+                email_template_data = email_template_data.replace('{app}', app_replacement_string)
                 email_template_fh.seek(0)
                 email_template_fh.truncate()
                 email_template_fh.write(email_template_data)
@@ -82,7 +96,7 @@ class NasNotifications(MethodView):
             if read_notify_email_file_rc:
                 email = EmailServices(subject=self.nas_down_email.get('subject').format(request.form.get('outage_start_date')),
                                       from_address=self.nas_down_email.get('from_addr'),
-                                      to_address=self.notify_email_dict.get('email_address_list'))
+                                      to_address=self.notify_email_dict.get('email_address_list'), app_instance=container_status_app)
 
                 with open(self.email_template_temp_file) as email_fh:
                     email_sent = email.send_email(email_fh.read())
@@ -93,8 +107,14 @@ class NasNotifications(MethodView):
                     container_status_app.logger.error(email_sent_message.format('not sent'))
 
                 if read_json_rc and nas_production_dict is not None:
-                    nas_production_dict['overall_status'] = 'NOT ACTIVE'
-                    nas_production_dict['restored_by'] = '{} {} {} CST'.format(request.form.get('outage_end_date'),
+                    if request.form.get('platform_name') == 'old_platform':
+                        overall_status = 'overall_status'
+                        restored_by = 'restored_by'
+                    else:
+                        overall_status = 'p2_overall_status'
+                        restored_by = 'p2_restored_by'
+                    nas_production_dict[overall_status] = 'NOT ACTIVE'
+                    nas_production_dict[restored_by] = '{} {} {} CST'.format(request.form.get('outage_end_date'),
                                                                                request.form.get('outage_end_time'),
                                                                                request.form.get('time_of_day_end'))
                     write_json_rc, file_updated = Common.rw_json_file(file_path=os.environ.get('container_status_path'),
@@ -119,8 +139,7 @@ class NasNotifications(MethodView):
         :param list_o_switch_name: switch names from the nas_notify.html components.
         :return:
         """
-        # read_json_rc, self.container_status_dict = Common.rw_json_file(file_path=os.environ.get('container_status_path'))
-        # if read_json_rc:
+
         if self.container_status_dict.get('nas_production') is not None:
             if self.container_status_dict['nas_production'].get('overall_status') is not None:
                 for switch_tuple in list_o_switch_name:
@@ -152,3 +171,64 @@ class NasNotifications(MethodView):
                 else:
                     container_status_app.logger.error(update_log_message)
         return
+
+    def put_p2(self, list_o_switch_name):
+        """
+        Called by the Post method if the Status Switch button is toggled on the New Architecture tab panel. Looks at
+        updating the container status JSON data to set the overall and each application status keys.
+
+        :param list_o_switch_name: switch names from the nas_notify.html components.
+        :return:
+        """
+
+        if self.container_status_dict.get('nas_production') is not None:
+            if self.container_status_dict['nas_production'].get('p2_overall_status') is not None:
+                for switch_tuple in list_o_switch_name:
+                    """
+                    For each switch tuple (i.e. (switch name, additional info) find the switch name in the request form 
+                    and see if it was switch on or off and set the appropriate status string.
+                    """
+                    if request.form.get(switch_tuple[0]) == 'on':
+                        update_status = 'ACTIVE'
+                    else:
+                        update_status = 'NOT ACTIVE'
+
+                    update_app_name = switch_tuple[0].split('_', 1)
+                    if update_app_name[0] == self.container_status_dict.get('nas_production').get('p2_display_name'):
+                        if update_status == 'ACTIVE':
+                            self.container_status_dict['nas_production']['p2_overall_status'] = update_status
+                    else:
+                        self.container_status_dict['nas_production']['new_platform_apps'][update_app_name[0]]['status'] = update_status
+                        self.container_status_dict['nas_production']['new_platform_apps'][update_app_name[0]]['additional_info'] = switch_tuple[1]
+
+                update_json_rc, file_updated = Common.rw_json_file(file_path=os.environ.get('container_status_path'),
+                                                                   mode='write',
+                                                                   output_dict=self.container_status_dict)
+
+                update_log_message = "{} update of container status JSON file. Path: {}".format(
+                    'Successful' if update_json_rc else 'Unsuccessful', file_updated)
+                if update_json_rc:
+                    container_status_app.logger.info(update_log_message)
+                else:
+                    container_status_app.logger.error(update_log_message)
+        return
+
+    @staticmethod
+    def create_list_of_toggle_names(application_names: list):
+        """
+        Given a the list of application names from the container_status_data JSON file, generates and return a list of
+        tuples containing (switch name, additional info textarea name)
+
+        :param application_names: list of applications names (i.e. ['B2B', 'Ethersam', 'etc...']
+        :return: list of tuples
+        """
+
+        toggle_switch_names = []
+
+        for app_name in application_names:
+            status_switch_name = app_name + '_status_switch'
+            additional_info_name = request.form.get(app_name + '_textarea')
+            app_toggle_info_tuple = (status_switch_name, additional_info_name)
+            toggle_switch_names.append(app_toggle_info_tuple)
+
+        return toggle_switch_names
